3f9900661894f95401eab0bcf846edb258b70d61
LAPTOP-NFJ9L1BM\Sum SokRon <sumsokron330@gmail.com>
2024-01-11 18:11:27 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main{
+
+    public class bankaccount  {String accountnumber; double balance; String CustomerName; String email; String Phonenumber; }
+    private Main ()
+    {
+    }
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj);
+    }
+}
